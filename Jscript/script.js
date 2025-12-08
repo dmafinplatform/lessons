c47615f4a8881b5c6df1d3ee0bcbe60459b634a4
Marat <dma.finplatform@gmail.com>
2025-12-08 09:47:10 +0300

    Java Script

---
+++ Jscript/script.js
@@ -0,0 +1,5 @@
+       console.log("Hello World!"); 
+       console.warn('Это предупреждение');
+       console.error('Это сообщение об ошибке');
+       alert('Dont forget to subscribe!');
+       
